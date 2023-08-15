e094fe530c85d9a445e009a14346cf646fe554d6
Leandro de Oliveira Torres Santos <leanots86@gmail.com>
2023-08-14 21:54:51 -0300

    Criação e configuração do workspace e projetos

---
+++ src/test/java/com/github/bb9leko/]foods/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.bb9leko.foods;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
